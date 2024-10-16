5442963c7b1dd75684932b11e829f34394836c8c
inniums <inniums@gmail.com>
2024-10-16 18:11:16 -0400

    health census

---
+++ healthAnalysis/health_contact.js
@@ -0,0 +1,6 @@
+const submitContactBtn = document.getElementById('submit-contact')
+submitContactBtn.addEventListener('click', sumbitContact);
+
+function sumbitContact(){
+    alert('Thank you for contacting us')
+}
